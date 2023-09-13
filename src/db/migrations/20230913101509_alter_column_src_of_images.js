8a26a11bbe7598c45418f15ab53ee290273fc5c9
Nicholas Njeru <nicholasnjeru917@gmail.com>
2023-09-13 13:16:13 +0300

    updates

---
+++ src/db/migrations/20230913101509_alter_column_src_of_images.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+   return knex.schema.alterTable('images', (table) => {
+        table.text('src').alter();
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.alterTable('images', (table) => {
+        table.string('src').alter();
+      });
+};
