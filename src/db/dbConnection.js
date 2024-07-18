99160f74adc6970928a3644a890be6195c793467
Nicco99 <nicholasnjeru917@gmail.com>
2024-07-19 00:35:26 +0300

    done

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -3,17 +3,17 @@ import mysql from "mysql2"
 
 dotenv.config();
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Nicco@6612",
-  database: "blif_investments_db",
-}); 
 // const db = mysql.createConnection({
-// host: "13.37.100.57",
-// user: "Nicco99", 
-// password: "Nicco@6612",
-// database: "blif_investments_db",
-// });
+//   host: "localhost",
+//   user: "root",
+//   password: "Nicco@6612",
+//   database: "blif_investments_db",
+// }); 
+const db = mysql.createConnection({
+host: "13.37.100.57",
+user: "Nicco99", 
+password: "Nicco@6612",
+database: "blif_investments_db",
+});
 
 export default db
